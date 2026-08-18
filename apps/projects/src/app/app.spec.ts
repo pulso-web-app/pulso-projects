d5c54cef077e0c8fcfff973d93013447eec943ee
Ewerton Mendes <ewertonmendes9@gmail.com>
2026-08-18 16:23:27 -0300

    chore: clean the project

--- apps/projects/src/app/app.spec.ts
+++ apps/projects/src/app/app.spec.ts
@@ -1,20 +1,29 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+
 import { App } from './app';
-import { NxWelcome } from './nx-welcome';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App, NxWelcome],
+      imports: [App],
+      providers: [provideRouter([])],
     }).compileComponents();
   });
 
-  it('should render title', async () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
+
+    expect(fixture.componentInstance).toBeTruthy();
+  });
+
+  it('should render the router outlet', () => {
+    const fixture = TestBed.createComponent(App);
+
+    fixture.detectChanges();
+
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome projects'
-    );
+
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
