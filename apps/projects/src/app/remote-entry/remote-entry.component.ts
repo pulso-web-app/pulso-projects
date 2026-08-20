42f7337fbaf77ccdc096cb78b1dd35f02f3162ff
Ewerton Mendes <ewertonmendes9@gmail.com>
2026-08-20 19:52:43 -0300

    refactor: remove remote entry component and associated styles

--- apps/projects/src/app/remote-entry/remote-entry.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'pulso-projects-remote-entry',
-  standalone: true,
-  templateUrl: './remote-entry.component.html',
-  styleUrl: './remote-entry.component.scss',
-})
-export class ProjectsRemoteEntryComponent {}
--- apps/projects/src/app/remote-entry/remote-entry.routes.ts
+++ apps/projects/src/app/remote-entry/remote-entry.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { ProjectsRemoteEntryComponent } from './remote-entry.component';
 
-export const PROJECTS_ROUTES: Routes = [
-  {
-    path: '',
-    component: ProjectsRemoteEntryComponent,
-  },
-];
+export const PROJECTS_ROUTES: Routes = [];
